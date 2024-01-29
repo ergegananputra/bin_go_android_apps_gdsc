b6542de4e3298020d5df3e6c48a08e75ccd2bf76
ergegananputra <ergegananputra@gmail.com>
2024-01-29 23:49:54 +0700

    Profile Interface Programatic Prepare Documentation

--- app/src/main/java/com/gdsc/bingo/ui/profil/ProfilFragment.kt
+++ app/src/main/java/com/gdsc/bingo/ui/profil/ProfilFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.gdsc.bingo.R
+import android.widget.Toast
 import com.gdsc.bingo.databinding.FragmentProfilBinding
 
 
@@ -20,12 +20,85 @@ class ProfilFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         // Inflate the layout for this fragment
+        preLoad()
         return binding.root
+    }
+
+    /**
+     * [preLoad]
+     *
+     *
+     * Digunakan untuk melakukan load data yang berukuran ringan sebelum fragment dibuat.
+     * Harap dicatat bahwa jika melakukan load data yang berukuran besar, maka akan memperlambat
+     * pembuatan fragment.
+     */
+    private fun preLoad() {
+        // TODO : ambil shared preference dan ganti value point, nama dan email
+        val sharedPreferences : Any? = null
+        val nama = "Todo: Ambil Nama"
+        val email = "Todo: Ambil Email"
+        val point = 0 // TODO : ambil point dari shared preference
+
+        setupTextName(nama)
+        setupTextEmail(email)
+        setTextPoint(point)
+    }
+
+    private fun setTextPoint(point: String) {
+        // TODO : Format point
+        binding.profilCardBinPointsTextViewPoints.text = point
+    }
+
+    private fun setTextPoint(point: Int) {
+        setTextPoint(point.toString())
+    }
+
+    private fun setupTextEmail(email: String) {
+        // TODO : ambil email dari shared preference
+        binding.profileCardProfileTextViewEmail.text = email
+    }
+
+    private fun setupTextName(nama: String) {
+        // TODO : ambil nama dari shared preference
+        binding.profilCardProfilTextViewName.text = nama
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupCardProfil()
+        setupCardProfilPicture()
+        setupBinPoints()
+    }
+
+    private fun setupBinPoints() {
+        // TODO : aksi klik untuk history point
+        binding.profilCardBinPoints.setOnClickListener {
+            Toast.makeText(requireContext(), "Todo: Aksi klik untuk history point", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setupCardProfilPicture() {
+        // TODO : load gambar dari storage dan aksi edit ketika klik
+
+        loadOrRefreshPicture()
+        binding.profilCardProfilPlaceholderImage.setOnClickListener {
+            // TODO : aksi edit gambar
+            Toast.makeText(requireContext(), "Todo: Aksi edit gambar", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun loadOrRefreshPicture() {
+        // TODO : load gambar dari storage ke
+        // binding.profilCardProfilImage
+        Toast.makeText(requireContext(), "Todo: Load gambar dari storage", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setupCardProfil() {
+        binding.profilCardProfilContainer.setOnClickListener {
+            // TODO: aksi login atau logout atau form edit profil
+            Toast.makeText(requireContext(), "Todo: Aksi login atau logout", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }

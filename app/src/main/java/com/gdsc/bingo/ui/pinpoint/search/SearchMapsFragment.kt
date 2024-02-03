bd98e14c9f7466b6be435f294f31a012a4d13b2a
aldyardnsyh <aldyardiansyah@mail.ugm.ac.id>
2024-02-03 22:23:42 +0700

    Setting Maps by Current Location

--- app/src/main/java/com/gdsc/bingo/ui/pinpoint/search/SearchMapsFragment.kt
+++ app/src/main/java/com/gdsc/bingo/ui/pinpoint/search/SearchMapsFragment.kt
@@ -1,5 +1,6 @@
 package com.gdsc.bingo.ui.pinpoint.search
 
+import android.annotation.SuppressLint
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
@@ -15,6 +16,14 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class SearchMapsFragment : Fragment() {
 
@@ -22,19 +31,47 @@ class SearchMapsFragment : Fragment() {
         FragmentSearchMapsBinding.inflate(layoutInflater)
     }
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastKnownLocation: Location
+
+    @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Inisialisasi FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        // Cek izin lokasi
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Mendapatkan lokasi terakhir
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    lastKnownLocation = location
+
+                    // Menyesuaikan kamera ke lokasi pengguna
+                    val currentLatLng = LatLng(location.latitude, location.longitude)
+                    googleMap.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(
+                            currentLatLng,
+                            DEFAULT_ZOOM
+                        )
+                    )
+                    // Menambahkan marker ke lokasi pengguna
+                    googleMap.addMarker(
+                        MarkerOptions().position(currentLatLng)
+                            .title("Your Current Location")
+                    )
+                }
+            }
+        } else {
+            // Jika izin lokasi tidak diberikan, minta izin
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
     }
 
     override fun onCreateView(
@@ -47,7 +84,32 @@ class SearchMapsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
+        val fabCompass: FloatingActionButton = view.findViewById(R.id.fabCompass)
+        val fabMyLocation: FloatingActionButton = view.findViewById(R.id.fabMyLocation)
+
+        fabCompass.setOnClickListener {
+            TODO("Not yet implemented")
+            // Implementasikan aksi yang diambil saat FAB Compass diklik
+            // Misalnya, arahkan peta ke utara atau lakukan operasi kompas
+            // ...
+
+        }
+
+        fabMyLocation.setOnClickListener {
+            TODO("Not yet implemented")
+            // Implementasikan aksi yang diambil saat FAB MyLocation diklik
+            // Misalnya, arahkan peta ke lokasi pengguna saat ini
+            // ...
+
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_ZOOM = 15f
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
 }

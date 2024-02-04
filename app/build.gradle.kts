9a8f99a093555f8ced68803ffa674705098e64fb
ergegananputra <ergegananputra@gmail.com>
2024-02-04 20:58:51 +0700

    Add Storage API dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,10 @@ plugins {
 
     // Navigation
     id("androidx.navigation.safeargs.kotlin")
+
+    // Google
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -81,6 +84,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-location:21.1.0")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

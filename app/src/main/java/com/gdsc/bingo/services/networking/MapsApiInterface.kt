2f999f4d7b6ae06930a1c772a6a3273fa360e20a
ergegananputra <126530940+ergegananputra@users.noreply.github.com>
2024-04-23 21:45:49 +0700

    Core : Merapikan kode

--- app/src/main/java/com/gdsc/bingo/services/networking/MapsApiInterface.kt
+++ app/src/main/java/com/gdsc/bingo/services/networking/MapsApiInterface.kt
@@ -1,20 +1,21 @@
 package com.gdsc.bingo.services.networking
 
-import com.gdsc.bingo.model.response.ModelResultsDetail
-import retrofit2.http.GET
-import com.gdsc.bingo.model.response.ModelResultsNearby
+import com.gdsc.bingo.model.response.ModelResultsDetailResponse
+import com.gdsc.bingo.model.response.ModelResultsNearbyResponse
 import retrofit2.Call
+import retrofit2.http.GET
 import retrofit2.http.Query
-interface ApiInterface {
+
+interface MapsApiInterface {
     @GET("place/nearbysearch/json")
     fun getDataResult(
         @Query("key") key: String,
         @Query("keyword") keyword: String,
         @Query("location") location: String,
         @Query("rankby") rankby: String
-    ): Call<ModelResultsNearby>
+    ): Call<ModelResultsNearbyResponse>
 
     @GET("/place/details/json")
     fun getDetailResult(@Query("key") key: String,
-                        @Query("placeid") placeid: String): Call<ModelResultsDetail>
+                        @Query("placeid") placeid: String): Call<ModelResultsDetailResponse>
 }
--- app/src/main/java/com/gdsc/bingo/ui/pinpoint/search/viewmodel/SearchMapsViewModel.kt
+++ app/src/main/java/com/gdsc/bingo/ui/pinpoint/search/viewmodel/SearchMapsViewModel.kt
@@ -5,70 +5,137 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.gdsc.bingo.BuildConfig.MAPS_API_KEY
-import com.gdsc.bingo.model.nearby.ModelResults
-import com.gdsc.bingo.model.response.ModelResultsNearby
-import com.gdsc.bingo.services.networking.ApiInterface
+import com.gdsc.bingo.model.BinLocation
 import com.gdsc.bingo.services.networking.ApiService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.util.*
+import com.gdsc.bingo.services.networking.MapsApiInterface
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 class SearchMapsViewModel : ViewModel() {
-    private val modelResultsMutableLiveData = MutableLiveData<ArrayList<ModelResults>>()
-    private val strApiKey = MAPS_API_KEY
+    private val _modelResultsMutableLiveData = MutableLiveData<ArrayList<BinLocation>>()
+    val modelResultsMutableLiveData: LiveData<ArrayList<BinLocation>>
+        get() = _modelResultsMutableLiveData
+
+//    private val strApiKey = MAPS_API_KEY
 
     fun setMarkerLocation(strLocation: String) {
-        val apiService: ApiInterface = ApiService.getMaps()
-
-        val combinedResults = ArrayList<ModelResults>()
+        val mapsApiService: MapsApiInterface = ApiService.getMaps()
+//
+//        val combinedResults = ArrayList<ModelResults>()
 
         // Hitung jumlah panggilan API yang telah selesai
-        var completedCalls = 0
+//        var completedCalls = 0
 
-        // Mencari Tempat Pembuangan
-        val callTePeEs = apiService.getDataResult(strApiKey, "Tempat Pembuangan Sampah", strLocation, "distance")
-        callTePeEs.enqueue(object : Callback<ModelResultsNearby> {
-            override fun onResponse(call: Call<ModelResultsNearby>, response: Response<ModelResultsNearby>) {
-                if (response.isSuccessful && response.body() != null) {
-                    combinedResults.addAll(response.body()!!.modelResults)
-                    completedCalls++
+//        // Mencari Tempat Pembuangan
+//        val callTePeEs = apiService.getDataResult(strApiKey, "Tempat Pembuangan Sampah", strLocation, "distance")
+//        callTePeEs.enqueue(object : Callback<ModelResultsNearbyResponse> {
+//            override fun onResponse(call: Call<ModelResultsNearbyResponse>, response: Response<ModelResultsNearbyResponse>) {
+//                if (response.isSuccessful && response.body() != null) {
+//                    combinedResults.addAll(response.body()!!.modelResults)
+//                    completedCalls++
+//
+//                    // Jika kedua panggilan telah selesai, maka kita bisa mem-post data ke modelResultsMutableLiveData
+//                    if (completedCalls == 2) {
+//                        modelResultsMutableLiveData.postValue(combinedResults)
+//                    }
+//                } else {
+//                    Log.e("responseTePeEs", response.toString())
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<ModelResultsNearbyResponse>, t: Throwable) {
+//                Log.e("failureTePeEs", t.toString())
+//            }
+//        })
+//
+//        // Mencari TPS
+//        val callTPS = apiService.getDataResult(strApiKey, "TPS", strLocation, "distance")
+//        callTPS.enqueue(object : Callback<ModelResultsNearbyResponse> {
+//            override fun onResponse(call: Call<ModelResultsNearbyResponse>, response: Response<ModelResultsNearbyResponse>) {
+//                if (response.isSuccessful && response.body() != null) {
+//                    combinedResults.addAll(response.body()!!.modelResults)
+//                    completedCalls++
+//
+//                    // Jika kedua panggilan telah selesai, maka kita bisa mem-post data ke modelResultsMutableLiveData
+//                    if (completedCalls == 2) {
+//                        modelResultsMutableLiveData.postValue(combinedResults)
+//                    }
+//                } else {
+//                    Log.e("responseTPS", response.toString())
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<ModelResultsNearbyResponse>, t: Throwable) {
+//                Log.e("failureTPS", t.toString())
+//            }
+//        })
 
-                    // Jika kedua panggilan telah selesai, maka kita bisa mem-post data ke modelResultsMutableLiveData
-                    if (completedCalls == 2) {
-                        modelResultsMutableLiveData.postValue(combinedResults)
-                    }
-                } else {
-                    Log.e("responseTePeEs", response.toString())
+        CoroutineScope(Dispatchers.Default).launch {
+            val queryTempatPembuanganSampah = withContext(Dispatchers.IO) {
+                searchPlaceMaps("Tempat Pembuangan Sampah", mapsApiService, strLocation) ?: emptyList()
+            }
+            val queryTPS = withContext(Dispatchers.IO) {
+                searchPlaceMaps("TPS", mapsApiService, strLocation) ?: emptyList()
+            }
+            val queryBankSampah = withContext(Dispatchers.IO) {
+                searchPlaceMaps("Bank Sampah", mapsApiService, strLocation) ?: emptyList()
+            }
+
+            _modelResultsMutableLiveData.postValue(
+                arrayListOf<BinLocation>().apply {
+                    addAll(queryTempatPembuanganSampah)
+                    addAll(queryTPS)
+                    addAll(queryBankSampah)
                 }
-            }
+            )
 
-            override fun onFailure(call: Call<ModelResultsNearby>, t: Throwable) {
-                Log.e("failureTePeEs", t.toString())
-            }
-        })
 
-        // Mencari TPS
-        val callTPS = apiService.getDataResult(strApiKey, "TPS", strLocation, "distance")
-        callTPS.enqueue(object : Callback<ModelResultsNearby> {
-            override fun onResponse(call: Call<ModelResultsNearby>, response: Response<ModelResultsNearby>) {
-                if (response.isSuccessful && response.body() != null) {
-                    combinedResults.addAll(response.body()!!.modelResults)
-                    completedCalls++
-
-                    // Jika kedua panggilan telah selesai, maka kita bisa mem-post data ke modelResultsMutableLiveData
-                    if (completedCalls == 2) {
-                        modelResultsMutableLiveData.postValue(combinedResults)
-                    }
-                } else {
-                    Log.e("responseTPS", response.toString())
-                }
-            }
-
-            override fun onFailure(call: Call<ModelResultsNearby>, t: Throwable) {
-                Log.e("failureTPS", t.toString())
-            }
-        })
+        }
     }
 
-    fun getMarkerLocation(): LiveData<ArrayList<ModelResults>> = modelResultsMutableLiveData
+    private suspend fun searchPlaceMaps(
+        keyword: String,
+        mapsApiInterface: MapsApiInterface,
+        locationString : String,
+        rankBy : String = "distance"
+    ) : List<BinLocation>? {
+        val response = withContext(Dispatchers.IO) {
+            mapsApiInterface.getDataResult(
+                MAPS_API_KEY,
+                keyword,
+                locationString,
+                rankBy
+            ).execute()
+        }
+
+        if (response.isSuccessful.not()) {
+            Log.e("responseApiCall", response.toString())
+            return null
+        }
+
+        val data = response.body()?.modelResults ?: run {
+            Log.e("SeachMapsViewModel", "responseApiCall.body()?.modelResults is null")
+            return null
+        }
+
+        Log.i("SearchMapsViewModel", "searchPlaceMaps: $data done fetching data")
+
+        return data.map { modelResults ->
+            BinLocation(
+                referencePath = null,
+                name = modelResults.name,
+                address = modelResults.vicinity,
+                latitude = modelResults.modelGeometry.modelLocation.lat,
+                longitude = modelResults.modelGeometry.modelLocation.lng,
+                additionalInfo = mapOf(
+                    "place_id" to modelResults.placeId
+                ),
+                isOpen = modelResults.isOpen,
+                rating = modelResults.rating,
+                reviewCount = null
+            )
+        }
+    }
 }

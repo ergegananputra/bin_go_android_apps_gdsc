600b9a071c60f10eb9b72055e576fe496ae636a6
ergegananputra <126530940+ergegananputra@users.noreply.github.com>
2024-05-01 20:22:09 +0700

    Feat : New RTE for text-align

--- app/src/main/java/com/gdsc/bingo/services/textstyling/AddOnSpannableTextStyle.kt
+++ app/src/main/java/com/gdsc/bingo/services/textstyling/AddOnSpannableTextStyle.kt
@@ -2,12 +2,15 @@ package com.gdsc.bingo.services.textstyling
 
 import android.graphics.Color
 import android.graphics.Typeface
+import android.text.Layout
 import android.text.Spannable
 import android.text.SpannableStringBuilder
+import android.text.style.AlignmentSpan
 import android.text.style.QuoteSpan
 import android.text.style.RelativeSizeSpan
 import android.text.style.StyleSpan
 import android.text.style.UnderlineSpan
+import android.util.Log
 import com.mohamedrejeb.ksoup.html.parser.KsoupHtmlHandler
 import com.mohamedrejeb.ksoup.html.parser.KsoupHtmlParser
 import kotlin.math.max
@@ -53,6 +56,13 @@ class AddOnSpannableTextStyle {
                             unorderedListCounter++
                         }
                     }
+                    "p", "div" -> {
+                        val align = attributes["style"]
+                        Log.d("AddOnSpannableTextStyle", "get Align $align")
+                        if (align != null) {
+                            openTags.add("$name-$align")
+                        }
+                    }
                 }
             }
             .onCloseTag { name, isImplied ->
@@ -83,131 +93,155 @@ class AddOnSpannableTextStyle {
                 // Apply the appropriate styling based on the parent tag
                 val start = max(0, spanned.length - text.length)
                 val end = max(start, spanned.length)
-                when (openTags.lastOrNull()) {
-                    "h1" -> {
-                        spanned.setSpan(
-                            StyleSpan(Typeface.BOLD),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                        spanned.setSpan(
-                            RelativeSizeSpan(2f),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                    }
 
-                    "h2" -> {
-                        spanned.setSpan(
-                            StyleSpan(Typeface.BOLD),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                        spanned.setSpan(
-                            RelativeSizeSpan(1.5f),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                    }
+                openTags.forEach { tag ->
 
-                    "h3" -> {
-                        spanned.setSpan(
-                            StyleSpan(Typeface.BOLD),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                        spanned.setSpan(
-                            RelativeSizeSpan(1.17f),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                    }
+                    when (tag) {
+                        "h1" -> {
+                            spanned.setSpan(
+                                StyleSpan(Typeface.BOLD),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                            spanned.setSpan(
+                                RelativeSizeSpan(2f),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                        }
 
-                    "h4" -> {
-                        spanned.setSpan(
-                            StyleSpan(Typeface.BOLD),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                        spanned.setSpan(
-                            RelativeSizeSpan(1.12f),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                    }
+                        "h2" -> {
+                            spanned.setSpan(
+                                StyleSpan(Typeface.BOLD),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                            spanned.setSpan(
+                                RelativeSizeSpan(1.5f),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                        }
 
-                    "h5" -> {
-                        spanned.setSpan(
-                            StyleSpan(Typeface.BOLD),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                        spanned.setSpan(
-                            RelativeSizeSpan(1f),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                    }
+                        "h3" -> {
+                            spanned.setSpan(
+                                StyleSpan(Typeface.BOLD),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                            spanned.setSpan(
+                                RelativeSizeSpan(1.17f),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                        }
 
-                    "h6" -> {
-                        spanned.setSpan(
-                            StyleSpan(Typeface.BOLD),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                        spanned.setSpan(
-                            RelativeSizeSpan(0.83f),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                    }
+                        "h4" -> {
+                            spanned.setSpan(
+                                StyleSpan(Typeface.BOLD),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                            spanned.setSpan(
+                                RelativeSizeSpan(1.12f),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                        }
 
-                    "b" -> {
-                        spanned.setSpan(
-                            StyleSpan(Typeface.BOLD),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                    }
+                        "h5" -> {
+                            spanned.setSpan(
+                                StyleSpan(Typeface.BOLD),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                            spanned.setSpan(
+                                RelativeSizeSpan(1f),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                        }
 
-                    "i" -> {
-                        spanned.setSpan(
-                            StyleSpan(Typeface.ITALIC),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                    }
+                        "h6" -> {
+                            spanned.setSpan(
+                                StyleSpan(Typeface.BOLD),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                            spanned.setSpan(
+                                RelativeSizeSpan(0.83f),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                        }
 
-                    "u" -> {
-                        spanned.setSpan(
-                            UnderlineSpan(),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                    }
+                        "b" -> {
+                            spanned.setSpan(
+                                StyleSpan(Typeface.BOLD),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                        }
 
-                    "blockquote" -> {
-                        spanned.setSpan(
-                            QuoteSpan(Color.LTGRAY),
-                            start,
-                            end,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
+                        "i" -> {
+                            spanned.setSpan(
+                                StyleSpan(Typeface.ITALIC),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                        }
+
+                        "u" -> {
+                            spanned.setSpan(
+                                UnderlineSpan(),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                        }
+
+                        "blockquote" -> {
+                            spanned.setSpan(
+                                QuoteSpan(Color.LTGRAY),
+                                start,
+                                end,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                        }
+
+                        else -> {
+                            Log.d("AddOnSpannableTextStyle", tag)
+                            if (tag.startsWith("div-text")) {
+                                val align = when (tag.split(":").last().trim().dropLast(1)) {
+                                    "left" -> Layout.Alignment.ALIGN_NORMAL
+                                    "center" -> Layout.Alignment.ALIGN_CENTER
+                                    "right" -> Layout.Alignment.ALIGN_OPPOSITE
+                                    else -> null
+                                }
+                                if (align != null) {
+                                    spanned.setSpan(
+                                        AlignmentSpan.Standard(align),
+                                        start,
+                                        end,
+                                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                                    )
+                                }
+                            }
+                        }
                     }
                 }
             }

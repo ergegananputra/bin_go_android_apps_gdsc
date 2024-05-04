5dbb057c2608e56c64d8b774333472cb023821c4
ergegananputra <126530940+ergegananputra@users.noreply.github.com>
2024-05-04 10:04:54 +0700

    Fix: Privacy Policies and Panduan

--- app/src/main/java/com/gdsc/bingo/ui/profil/kebijakan_privasi/ProfilKebijakanPrivasiFragment.kt
+++ app/src/main/java/com/gdsc/bingo/ui/profil/kebijakan_privasi/ProfilKebijakanPrivasiFragment.kt
@@ -1,13 +1,13 @@
 package com.gdsc.bingo.ui.profil.kebijakan_privasi
 
+import android.graphics.Typeface
 import android.os.Bundle
-import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import com.gdsc.bingo.databinding.FragmentProfilKebijakanPrivasiBinding
+import com.gdsc.bingo.services.textstyling.AddOnSpannableTextStyle
 
 class ProfilKebijakanPrivasiFragment : Fragment() {
 
@@ -30,40 +30,58 @@ class ProfilKebijakanPrivasiFragment : Fragment() {
             <br><br>
             <div style="text-align: center;"><h2><b>User Policy for BINGO</b></h2></div><br><br>
             
-            <div class="policy-text">
+            
                 <b>Welcome to BINGO!</b> We are delighted to have you as part of our community. Before you start using our app, please take a moment to review our user policy. By accessing or using BINGO, you agree to comply with this policy.<br><br>
             
-                <b>1. Account Registration:</b><br>
-                • To access certain features of BINGO, you may need to create an account. When registering, you agree to provide accurate and complete information.<br>
-                • You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account.<br><br>
+                <h3>1. Account Registration:</h3><br>
+                <ul>
+                <li>To access certain features of BINGO, you may need to create an account. When registering, you agree to provide accurate and complete information.</li>
+                <li>You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account.</li>
+                </ul><br>
             
-                <b>2. App Usage:</b><br>
-                • BINGO is designed to promote responsible waste disposal practices and community engagement in waste management initiatives.<br>
-                • Users must not use BINGO for any unlawful, harmful, or inappropriate purposes. This includes but is not limited to: posting offensive content, engaging in illegal activities, or violating the rights of others.<br>
-                • Users are encouraged to engage respectfully with other community members and contribute constructively to discussions.<br><br>
+                <h3>2. App Usage:</h3><br>
+                <ul>
+                <li>BINGO is designed to promote responsible waste disposal practices and community engagement in waste management initiatives.</li>
+                <li>Users must not use BINGO for any unlawful, harmful, or inappropriate purposes. This includes but is not limited to: posting offensive content, engaging in illegal activities, or violating the rights of others.</li>
+                <li>Users are encouraged to engage respectfully with other community members and contribute constructively to discussions.</li>
+                </ul><br>
             
-                <b>3. Content Guidelines:</b><br>
-                • Users are responsible for the content they post on BINGO. Content must not violate any laws or infringe upon the rights of others.<br>
-                • Content that promotes hate speech, violence, discrimination, or illegal activities is strictly prohibited.<br>
-                • BINGO reserves the right to remove any content that violates these guidelines without prior notice.<br><br>
+                <h3>3. Content Guidelines:</h3><br>
+                <ul>
+                <li>Users are responsible for the content they post on BINGO. Content must not violate any laws or infringe upon the rights of others.</li>
+                <li>Content that promotes hate speech, violence, discrimination, or illegal activities is strictly prohibited.</li>
+                <li>BINGO reserves the right to remove any content that violates these guidelines without prior notice.</li>
+                </ul><br>
             
-                <b>4. Privacy and Data Security:</b><br>
-                • BINGO respects your privacy and is committed to protecting your personal information. Please refer to our Privacy Policy for details on how we collect, use, and disclose your data.<br>
-                • You agree not to misuse any personal information of other users obtained through BINGO.<br><br>
+                <h3>4. Privacy and Data Security:</h3><br>
+                <ul>
+                <li>BINGO respects your privacy and is committed to protecting your personal information. Please refer to our Privacy Policy for details on how we collect, use, and disclose your data.</li>
+                <li>You agree not to misuse any personal information of other users obtained through BINGO.</li>
+                </ul><br>
             
-                <b>5. Reporting Violations:</b><br>
-                • If you encounter any violations of this user policy or observe inappropriate behavior on BINGO, please report it to us immediately.<br>
-                • BINGO will investigate reported violations and take appropriate action, including removing offensive content and suspending or terminating accounts.<br><br>
+                <h3>5. Reporting Violations:</h3><br>
+                <ul>
+                <li>If you encounter any violations of this user policy or observe inappropriate behavior on BINGO, please report it to us immediately.</li>
+                <li>BINGO will investigate reported violations and take appropriate action, including removing offensive content and suspending or terminating accounts.</li>
+                </ul><br>
             
-                <b>6. Changes to Policy:</b><br>
-                • BINGO reserves the right to update or modify this user policy at any time without prior notice. Any changes will be effective immediately upon posting.<br>
-                • It is your responsibility to review this policy periodically to stay informed of any updates.<br><br>
+                <h3>6. Changes to Policy:</h3><br>
+                <ul>
+                <li>BINGO reserves the right to update or modify this user policy at any time without prior notice. Any changes will be effective immediately upon posting.</li>
+                <li>It is your responsibility to review this policy periodically to stay informed of any updates.</li>
+                </ul><br><br>
             
                 Thank you for being part of the BINGO community! If you have any questions or concerns about this user policy, please contact us at <a href='mailto:bingo@gmail.com'>bingo@gmail.com</a>.
-            </div>
+            
             """.trimIndent()
 
-        binding.textViewUserPolicy.text = HtmlCompat.fromHtml(userPolicyText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+        val spannableConverter = AddOnSpannableTextStyle()
+
+        val spanned = spannableConverter.convertHtmlWithOrderedList(userPolicyText )
+
+        binding.textViewUserPolicy.text = spanned
+        val typeface = Typeface.DEFAULT
+        binding.textViewUserPolicy.typeface = typeface
     }
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/gdsc/bingo/ui/profil/panduan/ProfilPanduanFragment.kt
+++ app/src/main/java/com/gdsc/bingo/ui/profil/panduan/ProfilPanduanFragment.kt
@@ -1,12 +1,13 @@
 package com.gdsc.bingo.ui.profil.panduan
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import com.gdsc.bingo.databinding.FragmentProfilPanduanBinding
+import com.gdsc.bingo.services.textstyling.AddOnSpannableTextStyle
 
 class ProfilPanduanFragment : Fragment() {
 
@@ -29,42 +30,63 @@ class ProfilPanduanFragment : Fragment() {
             <br><br>
             <div style="text-align: center;"><h2><b>Panduan Penggunaan Bin-Go</b></h2></div>
             <br><br>
-            <b>Daftar atau log in akun Bin-Go</b><br>
-            1. Buka aplikasi Bin-Go<br>
-            2. Pergi ke halaman Profil melalui pintasan pada navigation bar<br>
-            3. Pilih ikon akun pada bagian atas halaman<br>
-            4. Pilih menu daftar apabila belum memiliki akun atau pilih menu login apabila sudah memiliki akun<br>
-            5. Daftar atau masuk dengan e-mail aktif. Jika mendaftar, pastikan semua kolom (nama pengguna, e-mail, kata sandi) terisi<br>
-            6. Klik daftar jika mendaftar atau klik log in jika masuk dengan akun yang sudah ada<br><br>
             
-            <b>Menggunakan fitur Bin-Locator</b><br>
-            1. Pastikan GPS Anda sudah aktif<br>
-            2. Pergi ke halaman Bin-Locator melalui card di halaman beranda atau pintasan di navigation bar<br>
-            3. Pilih menu Bin-Locator di bagian atas layar<br>
-            4. Navigasi tempat sampah terdekat berdasarkan GPS Anda atau cari tempat sampah dengan fitur search di bagian atas halaman<br>
-            5. Pilih ikon tempat sampah yang tersedia untuk melihat keterangan lebih lanjut mengenai tempat sampah, meliputi nama tempat, alamat, keterangan jam buka, rating, dan rute<br>
-            6. Tekan tombol rute untuk pergi ke Google Maps dan navigasi arah menuju tempat sampah<br><br>
+            <h3>Daftar atau log in akun Bin-Go</h3>
+            <ol>
+            <li>Buka aplikasi Bin-Go</li>
+            <li>Pergi ke halaman Profil melalui pintasan pada navigation bar</li>
+            <li>Pilih ikon akun pada bagian atas halaman</li>
+            <li>Pilih menu daftar apabila belum memiliki akun atau pilih menu login apabila sudah memiliki akun</li>
+            <li>Daftar atau masuk dengan e-mail aktif. Jika mendaftar, pastikan semua kolom (nama pengguna, e-mail, kata sandi) terisi</li>
+            <li>Klik daftar jika mendaftar atau klik log in jika masuk dengan akun yang sudah ada</li>
+            </ol>
             
-            <b>Memposting pengetahuan di Forum</b><br>
-            1. Pastikan Anda sudah log in atau mendaftar<br>
-            2. Tekan pilihan "berbagi ide dan pengalaman" di halaman beranda atau pergi ke halaman Forum melalui pintasan di navigation bar dan tekan tombol bertanda plus<br>
-            3. Pilih tipe informasi yang ingin dibagikan (Bin-Tricks untuk tips dan trik mengolah sampah atau Bin-Learn untuk informasi umum) kemudian isi judul dan detail postingan. Ceritakan pengalamanmu atau bagikan idemu dalam menangani sampah. Tambahkan media gambar atau video melalui link youtube bila perlu<br>
-            4. Tekan tombol bertanda silang di pojok kiri atas untuk membatalkan atau tombol bertanda centang di pojok kanan atas untuk memposting<br><br>
+            <h3>Menggunakan fitur Bin-Locator</h3>
+            <ol>
+            <li>Pastikan GPS Anda sudah aktif</li>
+            <li>Pergi ke halaman Bin-Locator melalui card di halaman beranda atau pintasan di navigation bar</li>
+            <li>Pilih menu Bin-Locator di bagian atas layar</li>
+            <li>Navigasi tempat sampah terdekat berdasarkan GPS Anda atau cari tempat sampah dengan fitur search di bagian atas halaman</li>
+            <li>Pilih ikon tempat sampah yang tersedia untuk melihat keterangan lebih lanjut mengenai tempat sampah, meliputi nama tempat, alamat, keterangan jam buka, rating, dan rute</li>
+            <li>Tekan tombol rute untuk pergi ke Google Maps dan navigasi arah menuju tempat sampah</li>
+            </ol>
             
-            <b>Melapor masalah sampah melalui Bin-Report</b><br>
-            1. Pastikan Anda sudah log in atau mendaftar<br>
-            2. Tekan pilihan untuk melapor dengan simbol pengeras suara di halaman beranda atau pergi ke halaman Forum melalui pintasan di navigation bar dan tekan tombol kecil dengan simbol pengeras suara<br>
-            3. Pilih titik lokasi dari google maps dan tekan simpan kemudian isi judul dan detail laporan. Ceritakan masalahmu dan tambahkan gambar bila perlu<br>
-            4. Tekan tombol bertanda silang di pojok kiri atas untuk membatalkan atau tombol bertanda centang di pojok kanan atas untuk memposting laporan<br><br>
+            <h3>Memposting pengetahuan di Forum</h3>
+            <ol>
+            <li>Pastikan Anda sudah log in atau mendaftar</li>
+            <li>Tekan pilihan "berbagi ide dan pengalaman" di halaman beranda atau pergi ke halaman Forum melalui pintasan di navigation bar dan tekan tombol bertanda plus</li>
+            <li>Pilih tipe informasi yang ingin dibagikan (Bin-Tricks untuk tips dan trik mengolah sampah atau Bin-Learn untuk informasi umum) kemudian isi judul dan detail postingan. Ceritakan pengalamanmu atau bag
+            ikan idemu dalam menangani sampah. Tambahkan media gambar atau video melalui link youtube bila perlu</li>
+            <li>Tekan tombol bertanda silang di pojok kiri atas untuk membatalkan atau tombol bertanda centang di pojok kanan atas untuk memposting</li>
+            </ol>
             
-            <b>Melihat Bin-Points</b><br>
-            1. Dalam sistem Bin-Go, Anda akan mendapat Bin-Points setelah memposting, melapor, atau menyelesaikan permasalahan sampah<br>
-            2. Pastikan Anda sudah log in atau mendaftar untuk mendapatkan Bin-Points<br>
-            3. Cek Bin-Points melalui menu Bin-Points di halaman beranda atau pergi ke halaman Profil melalui pintasan di navigation bar kemudian memilih menu Bin-Points di bawah informasi akun Anda<br>
-            4. Cek Bin-Points dan peringkat Anda di leaderboard. Pastikan Anda selalu berada di peringkat teratas untuk mendapatkan hadiah menarik<br>
+            <h3>Melapor masalah sampah melalui Bin-Report</h3>
+            <ol>
+            <li>Pastikan Anda sudah log in atau mendaftar</li>
+            <li>Tekan pilihan untuk melapor dengan simbol pengeras suara di halaman beranda atau pergi ke halaman Forum melalui pintasan di navigation bar dan tekan tombol kecil dengan simbol pengeras suara</li>
+            <li>Pilih titik lokasi dari google maps dan tekan simpan kemudian isi judul dan detail laporan. Ceritakan masalahmu dan tambahkan gambar bila perlu</li>
+            <li>Tekan tombol bertanda silang di pojok kiri atas untuk membatalkan atau tombol bertanda centang di pojok kanan atas untuk memposting laporan</li>
+            </ol>
+            
+            <h3>Melihat Bin-Points</h3>
+            <ol>
+            <li>Dalam sistem Bin-Go, Anda akan mendapat Bin-Points setelah memposting, melapor, atau menyelesaikan permasalahan sampah</li>
+            <li>Pastikan Anda sudah log in atau mendaftar untuk mendapatkan Bin-Points</li>
+            <li>Cek Bin-Points melalui menu Bin-Points di halaman beranda atau pergi ke halaman Profil melalui pintasan di navigation bar kemudian memilih menu Bin-Points di bawah informasi akun Anda</li>
+            <li>Cek Bin-Points dan peringkat Anda di leaderboard. Pastikan Anda selalu berada di peringkat teratas untuk mendapatkan hadiah menarik</li>
+            </ol>
+            
             """.trimIndent()
 
-        binding.textViewPanduan.text = HtmlCompat.fromHtml(panduanText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+
+        val spannableConverter = AddOnSpannableTextStyle()
+
+        val spanned = spannableConverter.convertHtmlWithOrderedList(panduanText )
+
+        binding.textViewPanduan.text = spanned
+        val typeface = Typeface.DEFAULT
+        binding.textViewPanduan.typeface = typeface
 
     }
 
